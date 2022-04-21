cdd5622f0052d279a16dc1b1e74345359a00cc9c
cpplv <aliepins@inbox.lv>
2022-04-21 15:10:26 +0300

    Add project files.

---
+++ WindowsFormsApplication_04/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 181);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pictures";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication_04/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int N = 0;
+        PictureBox[] P;
+        Random rrr = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int k, x, y;
+            for (k = 0; k < N; k++)
+                if (P[k].ImageLocation == "sun.jpg")
+                {
+                    do
+                    {
+                        x = P[k].Left + rrr.Next(-20, 20);
+                        y = P[k].Top + rrr.Next(-20, 20);
+                    }
+                    while (x < 10 || y < 10 || x > groupBox1.Width - 20 || y > groupBox1.Height - 20);
+                    P[k].Left = x;
+                    P[k].Top = y;
+                }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k;
+            try
+            {
+                N = int.Parse(textBox1.Text);
+            }
+            catch { };
+            if (N < 1) N = 1;
+            else if (N > 10) N = 10;
+            textBox1.Text = N.ToString();
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            P = new PictureBox[N];
+            for (k = 0; k < N; k++)
+            {
+                P[k] = new PictureBox();
+                P[k].Parent = groupBox1;
+                P[k].Width = 50;
+                P[k].Height = 50;
+                P[k].Top = rrr.Next(10, groupBox1.Height - 60);
+                P[k].Left = rrr.Next(10, groupBox1.Width - 60);
+                P[k].ImageLocation = "sun.jpg";
+                P[k].Load();
+                P[k].Click += new EventHandler(PicClick);
+            }
+            timer1.Enabled = true;
+        }
+
+        protected void PicClick(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            p.ImageLocation = "moon.jpg";
+            p.Load();
+            p.SendToBack();
+        }
+    }
+}
